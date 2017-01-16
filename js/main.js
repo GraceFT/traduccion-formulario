be748723bf54f889712f5a6d35240e5483862146
GraceF <gracefctorres@gmail.com>
2017-01-16 12:03:35 -0500

    last changes

---
+++ js/main.js
@@ -0,0 +1,18 @@
+ document.getElementById("form-signin-heading").innerHTML="Por favor inicia sesion";
+document.getElementById("inputEmail").placeholder="Correo Electronico";
+document.getElementById("inputPassword").placeholder="Contrasena";
+document.querySelector("span").innerHTML="Recordar datos";
+document.querySelector("button").innerHTML="Inicia Sesion";
+
+
+
+
+/*var button = document.createElement("button");
+    var text = document.createTextNode("Traducir");
+    button.appendChild(text);
+    document.body.appendChild(button);
+var button = function(){
+    
+    
+}*/
+    
